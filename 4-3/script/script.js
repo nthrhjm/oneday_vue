a2a4e396dee843b39de6da12ac1a70e8b8831350
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-08-01 22:50:56 +0900

    v-else-ifを使った分岐

---
+++ 4-3/script/script.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            count: 59
+        };
+    },
+    methods: {
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            this.count--;
+        },
+    },
+
+});
